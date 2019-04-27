a6f10f724905a1205f42efe52ff5fbbb777a9b3d
wuting <reruin@gmail.com>
2019-04-27 22:58:38 +0800

    fix display bug for gd

--- plugins/drive.gd.js
+++ plugins/drive.gd.js
@@ -13,8 +13,11 @@ const defaultProtocol = 'gd'
 
 const host = 'https://drive.google.com'
 
-module.exports = ({ request , getConfig , datetime , cache }) => {
+module.exports = ({ request , getConfig , datetime , cache , querystring }) => {
 
+  const createFetchParams = (id , key , maxResults = 99999) => {
+    return `openDrive=true&reason=102&syncType=0&errorRecovery=false&q=trashed%20%3D%20false%20and%20%27${id}%27%20in%20parents&fields=kind%2CnextPageToken%2Citems(kind%2CmodifiedDate%2CmodifiedByMeDate%2ClastViewedByMeDate%2CfileSize%2Cowners(kind%2CpermissionId%2CdisplayName%2Cpicture)%2ClastModifyingUser(kind%2CpermissionId%2CdisplayName%2Cpicture)%2ChasThumbnail%2CthumbnailVersion%2Ctitle%2Cid%2Cshared%2CsharedWithMeDate%2CuserPermission(role)%2CexplicitlyTrashed%2CmimeType%2CquotaBytesUsed%2Cshareable%2Ccopyable%2CfileExtension%2CsharingUser(kind%2CpermissionId%2CdisplayName%2Cpicture)%2Cspaces%2Ceditable%2Cversion%2CteamDriveId%2ChasAugmentedPermissions%2CcreatedDate%2CtrashingUser(kind%2CpermissionId%2CdisplayName%2Cpicture)%2CtrashedDate%2Cparents(id)%2Ccapabilities(canCopy%2CcanDownload%2CcanEdit%2CcanAddChildren%2CcanDelete%2CcanRemoveChildren%2CcanShare%2CcanTrash%2CcanRename%2CcanReadTeamDrive%2CcanMoveTeamDriveItem)%2Clabels(starred%2Chidden%2Ctrashed%2Crestricted%2Cviewed))%2CincompleteSearch&appDataFilter=NO_APP_DATA&spaces=drive&maxResults=${maxResults}&orderBy=folder%2Ctitle_natural%20asc&key=${key}`
+  }
   // gd folder => files
   const folder = async(id) => {
     let resid = `${defaultProtocol}:${id}`
@@ -35,45 +38,60 @@ module.exports = ({ request , getConfig , datetime , cache }) => {
 
 
     let { body } = await request.get(host+'/drive/folders/'+id)
-    let code = (body.match(/window\['_DRIVE_ivd'\]\s*=\s*'([^']+)'/) || ['',''])[1]
-    let data = code.replace(/\\x22/g,'"').replace(/\\x27/g,"'").replace(/\\x5b/g,'[').replace(/\\x5d/g,']').replace(/\\(r|n)/g,'').replace(/\\\\u/g,'\\u').toString(16)
-    // console.log(data)
-
-    if(data){
+    let code = (body.match(/__initData\s*=\s*([\w\W]+?);<\/script>/) || ['',''])[1]
+    code = code.replace(/[\r\n]/g,'')
+    let server , key;
+    if(code){
       try{
-        data = JSON.parse(data)
-        if(data.length){
-          data = data[0]
-        }
+        code = JSON.parse(code)
+        server = code[0][50][0]
+        key = code[0][9][32][35]
       }catch(e){
-        data = []
       }
     }
 
-    let children = data ? data.map((i)=>{
-      return {
-        id:i[0],
-        name:i[2],
-        ext:i[44],
-        protocol:defaultProtocol,
-        parent:i[1][0],
-        mime:i[3],
-        created_at:datetime(i[9]),
-        updated_at:datetime(i[10]),
-        size:parseInt(i[13]),
-        type : i[3].indexOf('.folder')>=0  ? 'folder' : undefined,
+    let data
+    if(server && key){
+      let qs = createFetchParams(id , key)
+      let url = `${server}/drive/v2beta/files?` + qs
+      let resp  = await request.get(url,{ headers:{'referer':`${host}/drive/folders/${id}`}})
+      if(resp.body){
+        try{
+          let r = JSON.parse(resp.body)
+          data = r.items;
+        }catch(e){
+        }
       }
-    }) : []
+    }
+
+    if( data ){
+      let children = data ? data.map((i)=>{
+        return {
+          id:i.id,
+          name:i.title,
+          ext:i.fileExtension,
+          protocol:defaultProtocol,
+          parent:id,
+          mime:i.mimeType,
+          created_at:datetime(i.createdDate),
+          updated_at:datetime(i.modifiedDate),
+          size:parseInt(i.fileSize),
+          type : i.mimeType.includes('.folder') ? 'folder' : undefined,
+        }
+      }) : []
 
 
-    //folder 额外保存 
-    resp.children = children
-    resp.$cached_at = Date.now()
+      //folder 额外保存 
+      resp.children = children
+      resp.$cached_at = Date.now()
 
-    cache.set(resid,resp)
-    return resp
-  }
-
+      cache.set(resid,resp)
+      return resp
+    }else{
+      return false
+    }
+    
+  } 
   /**
    * 获取文件实际路径
    */

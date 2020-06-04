a4bb2d1132bc9ad0f785254438a99f3f08868165
wuting <reruin@gmail.com>
2020-06-04 22:33:45 +0800

    fix: document preview

--- plugins/preview.document.js
+++ plugins/preview.document.js
@@ -37,12 +37,13 @@ module.exports = ({ getSource , getConfig, request }) => {
     }
   }
 
-  const createUrl = (req , withToken = false) => {
+  const createUrl = (req , withToken = false , append = {}) => {
     let query = req.query || {}
     delete query.preview
     if(withToken){
       query.token = getConfig('token')
     }
+    query = Object.assign(query , append)
     let querystr = qs.stringify(query)
     return req.origin + req.path + ( querystr ? ('?' + querystr) : '')
   }
@@ -61,11 +62,11 @@ module.exports = ({ getSource , getConfig, request }) => {
     }
 
     let rawUrl = createUrl(req,true)
-    rawUrl = 'https://sharelist.reruin.net/%E6%9C%AC%E5%9C%B0%E6%96%87%E4%BB%B6/preview/work.docx'
     let WOPISrc = encodeURIComponent(`http://us1-view-wopi.wopi.live.net:808/oh/wopi/files/@/wFileId?wFileId=${encodeURIComponent(rawUrl)}`)
-
     let url = `https://us1-word-view.officeapps.live.com/wv/wordviewerframe.aspx?ui=zh-CN&rs=zh-CN&WOPISrc=${WOPISrc}&access_token_ttl=0`
+
     let resp = await request.get(url,{ headers:{'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}})
+
     let previewUrl
     if(resp.body){
       let z = (resp.body.match(/(?<=z\=)[a-z0-9]+?(?=\')/i) || [''])[0]
@@ -76,7 +77,7 @@ module.exports = ({ getSource , getConfig, request }) => {
 
     if(previewUrl) {
       fileMap[req.path] = previewUrl
-      let url = req.origin + req.path + '?' + qs.stringify({ preview:1, conv:1 })
+      let url = createUrl(req , false , { preview:1, conv : 1})
       let body = `<iframe src="//mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(url)}"></iframe>`
       return {
         ...data,
@@ -95,12 +96,22 @@ module.exports = ({ getSource , getConfig, request }) => {
   }
 
   const pdf = async (data , req) => {
-    return {
-      ...data,
-      body: `
-        <iframe src="//mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(createUrl(req))}"></iframe>
-      `
+    if( req.query.conv ){
+      return {
+        ...data,
+        convertible:true,
+        outputType:'stream',
+        body:request({url:createUrl(req)})
+      }
+    }else{
+      return {
+        ...data,
+        body: `
+          <iframe src="//mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(createUrl(req , false , { preview:1, conv : 1}))}"></iframe>
+        `
+      }
     }
+
   }
 
   const preview = {};

820666514c1290b5ea9c9a33ecba38e5ea301130
Agni_Shine <shineagni@gmail.com>
2021-04-20 15:47:23 +0800

    - add aliyun drive thumb

--- plugins/drive.aliyundrive.js
+++ plugins/drive.aliyundrive.js
@@ -409,6 +409,7 @@ console.log(resp.body.items)
           created_at: i.created_at,
           updated_at: i.updated_at,
           type: i.type == 'folder' ? 'folder' : 'file',
+          thumb: i.thumbnail,
           $download_url:i.download_url,
           $cached_at: ts
         }
@@ -443,6 +444,7 @@ console.log(resp.body.items)
         ext: hit.ext,
         protocol: protocol,
         size: hit.size,
+        thumb: i.thumbnail,
         // $expired_at: expired_at,
         //proxy:true,
         headers:{
